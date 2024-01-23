dc66615a56d2d04be8c34d259394136aed795d8e
HARSH KUMAWAT <115946158+harshk04@users.noreply.github.com>
2024-01-23 17:57:08 +0530

    Added file

--- mail(without attachment).py
+++ mail(without attachment).py
@@ -2,9 +2,6 @@ import pandas as pd
 import smtplib as sm
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-import os
 
 df = pd.read_excel("/content/email.xlsx")
 email_col = df.get("Email")
@@ -31,34 +28,17 @@ try:
 
     from_ = email_user
     to_ = email_list
-    
+
     message = MIMEMultipart("alternative")
     message['Subject'] = subject_user
     message['From'] = from_
-
-    add_attachment=int(input("Do you want to add an attachment? (1 for Yes, 0 for No): "))
-
-
-    if add_attachment == 1:
-      fileName = "email.xlsx"
-      filePath = "/content/email.xlsx"
-      fileSize = os.path.getsize(filePath) / 10 ** 6
-
-      if fileSize > float(20):
-          print(fileName, "Can't Be Uploaded As The Size Is much large.")
-      else:
-          attachment = MIMEBase("application", "octet-stream")
-          attachment.set_payload(open(filePath, "rb").read())
-          encoders.encode_base64(attachment)
-          attachment.add_header('Content-Disposition', f'attachment; filename="{fileName}"')
-          message.attach(attachment)
 
     # Convert the user input HTML body to MIMEText
     html = MIMEText(html_body, "html")
     message.attach(html)
 
     server.sendmail(from_, to_, message.as_string())
-    print("Mails have been sent successfully.")
+    print("Mails have been sent successfully. ")
 
 except Exception as e:
     print(e)
